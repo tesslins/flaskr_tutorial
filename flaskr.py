75733df3c148364ab02004ea013c0068af27aa6f
Tess Bakke <tesslins@salvaje.gateway.sonic.net>
2014-06-29 23:19:47 -0700

    functions through Step 4

--- flaskr.py
+++ flaskr.py
@@ -29,6 +29,12 @@ def connect_db():
 	rv.row_factory = sqlite3.Row
 	return rv
 
+def init_db():
+	with app.app_context():
+		db = get_db()
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+			db.commit()
 @app.teardown_appcontext
 def close_db(error):
 	"""Closes the database again at the end of the request."""
