f6b05761cd9bbc39686096d50f72547e4d0e0356
Tess Bakke <tesslins@salvaje.gateway.sonic.net>
2014-06-29 23:15:03 -0700

    functions through step 3

--- flaskr.py
+++ flaskr.py
@@ -17,11 +17,23 @@ app.config.update(dict(
 	))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+def get_db():
+	"""Opens a new database connection if there is none yet for the current application context."""
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
 def connect_db():
 	"""Connects to the specific database"""
 	rv = sqlite3.connect(app.config['DATABASE'])
 	rv.row_factory = sqlite3.Row
 	return rv
+
+@app.teardown_appcontext
+def close_db(error):
+	"""Closes the database again at the end of the request."""
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
 
 if __name__ == '__main__':
 	app.run()
