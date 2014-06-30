4ce1de694501aeb1a46f3c48550f30d7557d09e6
Tess Bakke <tesslins@salvaje.gateway.sonic.net>
2014-06-29 23:05:58 -0700

    functions through step 2

--- flaskr.py
+++ flaskr.py
@@ -16,3 +16,12 @@ app.config.update(dict(
 	PASSWORD='default'
 	))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+	"""Connects to the specific database"""
+	rv = sqlite3.connect(app.config['DATABASE'])
+	rv.row_factory = sqlite3.Row
+	return rv
+
+if __name__ == '__main__':
+	app.run()
