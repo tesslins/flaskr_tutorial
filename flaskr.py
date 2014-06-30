62ba7b76fd482590b91b41b7826d7f631e06701e
Tess Bakke <tesslins@salvaje.gateway.sonic.net>
2014-06-29 22:55:45 -0700

    begining Step 2

---
+++ flaskr.py
@@ -0,0 +1,18 @@
+#all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+#create the f out of this application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+#load default config and override config from an environment variable
+app.config.update(dict(
+	DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+	DEBUG=True,
+	SECRET_KEY='development key',
+	USERNAME='admin',
+	PASSWORD='default'
+	))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
